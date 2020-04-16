8ea0554675e138b130b03dccf65d1cf383e17275
Neal McBurnett <neal@mcburnett.org>
2020-04-16 16:45:39 -0600

    feat: actually return round+1 for impossible kmin...

--- code/athena/athena.py
+++ code/athena/athena.py
@@ -225,7 +225,7 @@ class AthenaAudit():
 
             # this means that there are 0 chance of stopping in the given round -- the kmin is unreachable
             if kmin_found is False:
-                    kmins[round] = round_schedule[round]
+                    kmins[round] = round_schedule[round] + 1
                     prob_sum[round] =  prob_sum[round - 1]
                     prob_tied_sum[round] =  prob_tied_sum[round - 1]
 
