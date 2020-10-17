dfdb69d170a03f50b6b401882cf806b62ab922e8
filipzz <filip.zagorski@gmail.com>
2020-10-18 00:11:01 +0200

    fixed: test_athena.py

--- code/athena/test_athena.py
+++ code/athena/test_athena.py
@@ -1,9 +1,43 @@
 import json
 import os
 
+from athena.audit import Audit
 from athena.athena import AthenaAudit
+
 import numpy as np
 
+def set_ele(results):
+    election = {}
+    #election["alpha"] = risk_limit
+    election["delta"] = 1.0
+    election["name"] = "name"
+    contest_name = "pres"
+    #election["round_schedule"] = round_schedule
+    ballots_cast = sum(results)
+    election["ballots_cast"] = ballots_cast
+    election["total_ballots"] = ballots_cast
+    candidates = ["A", "B"]
+    #results = {1981473, 95369}
+    tally = {}
+    for can, votes in zip(candidates, results):
+        tally[can] = votes
+
+    #tallyj = json.dumps(tally)
+            #election["contests"] = f'{{"{contest_name}": {{"contest_ballots": {ballots_cast}, "tally": {tallyj}, "num_winners": {winners}, "reported_winners": ["A"]}} }}'
+            #election["data"] = f'{{"name": "x", "total_ballots": {ballots_cast}, "contests" : {election["contests"]}}}'
+    election["contests"] = {contest_name: {"contest_ballots": ballots_cast, "tally": tally, "num_winners": 1, "reported_winners": ["A"]}}
+    election["data"] = {"name": "x", "total_ballots": ballots_cast, "contests" : election["contests"]}
+            #print(election["contests"])
+            #json.loads(election["contests"])
+            #print(election["data"])
+            #print(election["data"])
+    election["candidates"] = candidates
+    election["results"] = results
+    election["winners"] = ["A"]
+    #election_object = Contest(election["data"])
+    #return election_object
+    #print(str(election))
+    return election
 
 def test_find_kmins():
 
@@ -32,7 +66,8 @@ def test_find_kmins():
         print("\n" + test + "\t" + str(ballots_cast) + "\t" + str(winner) + "\t" + str(round_schedule))
 
         #computed = schedule.find_new_kmins(ballots_cast, winner, alpha, round_schedule, risk_goal)
-        audit_object = AthenaAudit()
+        """
+        audit_object = AthenaAudit(audit_type, alpha, delta)
         if audit_type.lower() in {"bravo", "wald"}:
             computed = audit_object.bravo(margin, alpha, round_schedule)
         elif audit_type.lower() == "arlo":
@@ -43,6 +78,10 @@ def test_find_kmins():
             computed = audit_object.minerva(margin, alpha, delta, round_schedule)
         else:
             computed = audit_object.athena(margin, alpha, delta, round_schedule)
+        """
+
+        audit_object = AthenaAudit(audit_type, alpha, delta)
+        computed = audit_object.audit(margin, round_schedule)
 
         kmins = computed["kmins"]
         prob_sum = computed["prob_sum"]
