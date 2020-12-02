0707b465e48221113a7d367db594d8f01a252151
filipzz <filip.zagorski@gmail.com>
2020-12-02 01:15:10 +0100

    v0.8.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 
 setup(
     name='athena',
-    version='0.8.2',
+    version='0.8.3',
     packages=find_packages('code'),
     package_dir={'': 'code'},    
     py_modules=[splitext(basename(path))[0] for path in glob('code/athena/*.py')],
